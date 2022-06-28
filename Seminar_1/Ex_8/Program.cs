f95eff662c8ebc9f235ba630696df87790220920
Alexander <063@mail.ru>
2022-06-29 02:05:15 +0400

    Задание 8

---
+++ Seminar_1/Ex_8/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Задача 8: Напишите программу, которая на вход принимает число (N),
+//  а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int EvenNumber = 2;
+
+while (EvenNumber <= N)
+{
+    Console.WriteLine(EvenNumber);
+    EvenNumber += 2;
+}
