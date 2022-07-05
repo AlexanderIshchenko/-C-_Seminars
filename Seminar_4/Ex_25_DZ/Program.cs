8ec758d423286f20f42e35ff7d50ef08eb09a058
Alexander <063@mail.ru>
2022-07-05 14:54:34 +0400

    Домашнее задание 25 семинар 4 верный вариант

--- Seminar_4/Ex_25_DZ/Program.cs
+++ Seminar_4/Ex_25_DZ/Program.cs
@@ -4,9 +4,20 @@
 // 2, 4 -> 16
 
 
+// Console.Write("Введите число A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Число А в степени В: " + Math.Pow(A, B));
+
+// Нужно написать цикл. Пропустил этот момент в варианте выше.
 Console.Write("Введите число A: ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int B = Convert.ToInt32(Console.ReadLine());
-Console.Write("Число А в степени В: " + Math.Pow(A, B));
-
+int result = A;
+for (int i = 1; i < B; i++)
+{
+    result*=A;
+}
+Console.Write("Число А в степени В: " + result);
