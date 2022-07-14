3a15628742bfb8b3de10effd28de722a9d89b262
Alexander <063@mail.ru>
2022-07-14 04:28:28 +0400

    Домашнее задание 34 семинар 5

---
+++ Seminar_5/Ex_34_DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задача 34: Задайте массив заполненный случайными 
+//  положительными трёхзначными числами. 
+//  Напишите программу, которая покажет количество 
+//  чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = new int[4];
+
+int countEvenNumbers = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(99,1000);
+    if (array[i] %2 == 0)
+    {
+        countEvenNumbers++;
+    }
+}
+Console.WriteLine($"Исходный массив: [{String.Join("; ", array)}], количество чётных чисел: {countEvenNumbers}");
