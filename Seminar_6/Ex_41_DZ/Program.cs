28904dde132e4f1bbd6fce43df0102bd0a9b04a7
Alexander <063@mail.ru>
2022-07-19 03:27:05 +0400

    Домашнее задание 41 Семинар 6

---
+++ Seminar_6/Ex_41_DZ/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите числа через запятую: ");
+int[] arrayNumbers = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+int countPositiveNumbers = 0;
+for (int i = 0; i < arrayNumbers.Length; i++)
+{
+    if (arrayNumbers[i] > 0) countPositiveNumbers ++;
+}
+Console.WriteLine($"Введено чисел больше 0: {countPositiveNumbers}");
