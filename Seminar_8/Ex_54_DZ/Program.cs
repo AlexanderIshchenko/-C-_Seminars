bf601610a54c10a6878d7219047647003419156e
Alexander <063@mail.ru>
2022-07-22 18:59:44 +0400

    Домашнее задание 54 семинар 8

---
+++ Seminar_8/Ex_54_DZ/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки 
+// двумерного массива.
+
+int[,] GetArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = GetArray(3, 5);
+
+PrintArray(array);
+
+Console.WriteLine("Результат: ");
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = j + 1; k < array.GetLength(1); k++)
+        {
+            if (array[i, j] < array[i, k])
+            {
+                int temp = array[i, j];
+                array[i, j] = array[i, k];
+                array[i, k] = temp;
+            }
+        }
+    }
+}
+
+PrintArray(array);
